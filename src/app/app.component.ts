149884da01405acfcef02cb69ce87887ef5047fd
rashi21 <rashisaxena5@gmail.com>
2022-01-23 22:09:06 +0530

    Adding functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { DemoData } from './demo-data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  
-  title = 'speedMartApp';
+  tasks = ['Toys Factory', 'Delivery', 'Legal', 'Disconnect'];
+  displayDeliveryScreen: boolean = false;
+  textToBeDisplayed: string = '';
+  optionIsClicked: boolean = false;
+
+  demoData = DemoData;
+
+  itemSelected(task: string): void {
+    this.optionIsClicked = true;
+    this.displayDeliveryScreen = false;
+    if (task == this.tasks[1]) {
+      this.textToBeDisplayed = '';
+      this.displayDeliveryScreen = true;
+    } else
+      this.textToBeDisplayed = `${task} Menu option is clicked and work is on progress...`;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {TableModule} from 'primeng/table';
+import {PaginatorModule} from 'primeng/paginator';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TableModule,
+    PaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
